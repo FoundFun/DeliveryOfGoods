a978215628b099da53b6422561398cc40c0194bd
Anatoly Ponomarev <i@foundfun.ru>
2023-08-10 15:58:52 +0500

    Deleted unused strings, connected the sound toggle

--- Assets/Source/Game/Scripts/DeliverPoint.cs
+++
@@ -1,5 +0,0 @@
-using UnityEngine;
-
-public class DeliverPoint : MonoBehaviour
-{
-}
--- Assets/Source/Game/Scripts/DontDestoy.cs
+++
@@ -1,9 +0,0 @@
-using UnityEngine;
-
-public class DontDestoy : MonoBehaviour
-{
-    private void Start()
-    {
-        DontDestroyOnLoad(gameObject);
-    }
-}
