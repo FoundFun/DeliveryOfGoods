df0c3e30a9c047163b11b49f6d3cd8f8a6d76a4c
Anatoly Ponomarev <i@foundfun.ru>
2023-10-03 19:11:37 +0500

    Add YandexAuthorization

---
+++ Assets/Source/Game/Scripts/Yandex/YandexAuthorization.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using UnityEngine;
+using Agava.YandexGames;
+using Source.Game.Scripts.View;
+
+namespace Source.Game.Scripts.Yandex
+{
+    public class YandexAuthorization : MonoBehaviour
+    {
+        [SerializeField] private YandexAuthorizationView _view;
+        
+        private Coroutine _coroutineAuthorize;
+
+        private void OnEnable() => 
+            _view.ClickButton += Authorize;
+
+        private void OnDisable() => 
+            _view.ClickButton -= Authorize;
+
+        public void Open() => 
+            _view.Open();
+
+        private void OnAuthorizeButtonClick() =>
+            PlayerAccount.Authorize();
+
+        private void OnRequestPersonalProfileDataPermissionButtonClick() =>
+            PlayerAccount.RequestPersonalProfileDataPermission();
+
+        private void Authorize()
+        {
+#if YANDEX_GAMES
+            if (_coroutineAuthorize != null)
+                StopCoroutine(_coroutineAuthorize);
+
+            _coroutineAuthorize = StartCoroutine(OnAuthorize());
+#endif
+        }
+
+        private IEnumerator OnAuthorize()
+        {
+            Close();
+            OnAuthorizeButtonClick();
+
+            yield return new WaitUntil(() => PlayerAccount.IsAuthorized);
+
+            OnRequestPersonalProfileDataPermissionButtonClick();
+        }
+
+        private void Close() => 
+            _view.Close();
+    }
+}
